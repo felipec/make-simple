2039b9d1fa4450a8cff64154122947e09c2b0f87
Felipe Contreras <felipe.contreras@gmail.com>
2023-03-17 19:14:05 -0600

    More stuff

--- pwsh.ps1
+++ pwsh.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/pwsh
 
 (Get-Command bash).Path
+(Get-Command sh).Path
+(Get-Command bash.exe).Path
+(Get-Command sh.exe).Path
 $Env:PATH
+bash sub/foo.t
+Start-Process -FilePath "bash" -Wait -NoNewWindow -ArgumentList "sub/foo.t"
