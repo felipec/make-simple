926c501d6a9fac2c8c5fcb0a0bc97c5566fce9b9
Felipe Contreras <felipe.contreras@gmail.com>
2025-08-22 17:56:37 -0600

    create-process: get exit code

--- create-process.c
+++ create-process.c
@@ -9,5 +9,10 @@ int main(int argc, char *argv[]) {
 	if (!CreateProcess(NULL, argv[1], NULL, 0, TRUE, 0, NULL, 0, &si, &pi))
 		return 1;
 
-	return WaitForSingleObject(pi.hProcess, INFINITE);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	DWORD r;
+	GetExitCodeProcess(pi.hProcess, &r);
+
+	return r;
 }
